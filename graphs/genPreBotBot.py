13c40d9987aa157a15dd0dce54e3f40619399111
Kameron Decker Harris <kamdh@uw.edu>
2014-07-22 13:07:08 -0700

    added genPreBotBot.py

--- graphs/genPreBotBot.py
+++ graphs/genPreBotBot.py
@@ -5,15 +5,18 @@ import numpy as np
 import networkx as nx
 
 
-n = 300
-pMatE = np.array([ (2.95/(n-1), 0.05/(n-1)), 
-                   (0.05/(n-1), 2.95/(n-1)) ])
-pMatI = np.array([ (1.0/(n-1), 2.0/(n-1)), 
-                   (2.0/(n-1), 1.0/(n-1)) ])
+n = 200
 pI = 0.2
 gE = 2.5
 gI = 2.5
 pTypes = [0, 0.25, 0.45, 0.3]
+a = 2.0
+b = 1.0
+c = 2.0
 
+pMatE = np.array([(a, b),
+                  (b, a)]) / (n-1)
+pMatI = np.array([(a, c),
+                  (c, a)]) / (n-1)
 g = respirnet.er_prebot_bot(n, pMatI, pMatE, pTypes, pI, gE, gI)
-nx.write_gml(g, 'test5.gml')
+nx.write_gml(g, 'test_prebot_bot.gml')
